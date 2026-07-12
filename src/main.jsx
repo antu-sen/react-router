19a594437cbe788a67517e837066b052ea7fbbea
Antu Sen <antusen787@gmail.com>
2026-07-12 13:28:51 +0600

    create about router

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-2xl text-cyan-500 text-center">Hello react router</div>,
+  },
+
+  {
+    path: "/about",
+    element: <div className="text-4xl text-amber-500 text-center">About Page</div>
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+   </StrictMode>
+);

0c86711e64c7927d854e0f05ac5dcfdc581877b0
Antu Sen <antusen787@gmail.com>
2026-07-14 12:29:19 +0600

    create footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,21 @@
+import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const root = () => {
+const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
+
+            <main>
+                <Outlet></Outlet>
+            </main>
+
+            <Footer/>
+
+
         </div>
     );
 };
 
-export default root;
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./root";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: ,
+    element: <Root></Root>,
   },
-
-  {
-    path: "/about",
-    element: <div className="text-4xl text-amber-500 text-center">About Page</div>
-  },
-
-  {
-    path: "/products",
-    element : <div className="text-4xl text-red-500 text-center">Products Page</div>
-  }
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
-   </StrictMode>
+  </StrictMode>
 );
